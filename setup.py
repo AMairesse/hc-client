fca10823de9922be32d78ff4729420e669cd0a27
Antoine Mairesse <antoine.mairesse@free.fr>
2015-08-18 19:23:23 +0200

    fix setup scripts

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='HC-Client',
-    version='0.6.0',
+    version='0.9.0',
     author='Antoine Mairesse',
     author_email='antoine.mairesse@free.fr',
     packages=find_packages(exclude=['ez_setup']),
     url='',
     license='COPYING',
     description='Client for Heat Control',
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
     requires=['RPi', 'requests', 'json', 'pytz', 'dateutil'],
     classifiers=[
         "Development Status :: 4 - Beta",
